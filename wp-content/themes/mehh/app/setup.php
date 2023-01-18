8fbcf4d81ee5724d9a546b900fe82542e4bd2b97
Madis Luik <madis.luik@eklips.se>
2023-01-18 15:34:15 +0200

    change to Lora font

--- wp-content/themes/mehh/app/setup.php
+++ wp-content/themes/mehh/app/setup.php
@@ -192,7 +192,7 @@ add_action('wp_head', function () { ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet">
 
     <?php
 }, 100);
