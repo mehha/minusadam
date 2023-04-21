bb2a8323b3f529b2c94f3540791cb820587e0789
Madis Luik <madis.luik@eklips.se>
2023-04-21 13:41:23 +0300

    paypal plugin

---
+++ wp-content/plugins/pymntpl-paypal-woocommerce/build/js/frontend-commons.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '129137170bae9d79ec0c');
---
+++ wp-content/plugins/pymntpl-paypal-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bba7a3c7a8f5c69efeef3911472a6cf4e4eee5e2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bba7a3c7a8f5c69efeef3911472a6cf4e4eee5e2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'paymentplugins/paypal-php-sdk' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8a0f17207db40f68741e8b8ae38024aa3236210c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../paymentplugins/paypal-php-sdk',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
