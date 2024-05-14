fa6f16ff05320e12be098eb2bd2ce969890509fd
Madis Luik <madis@eklips.se>
2024-05-14 16:58:18 +0300

    WC fn if

--- wp-content/themes/mehh/app/helpers.php
+++ wp-content/themes/mehh/app/helpers.php
@@ -10,6 +10,10 @@ add_filter('body_class', function($classes){
     global $post;
     if (is_page() && get_field('wide_layout', $post->ID)) $classes[] = 'wide-layout';
     if ((is_single() || is_page()) && has_post_thumbnail()) $classes[] = 'has-top-image';
-    if (is_product_category() || is_shop()) $classes[] = 'has-primary-sidebar';
+
+    if ( function_exists( 'WC' ) ) {
+        if (is_product_category() || is_shop()) $classes[] = 'has-primary-sidebar';
+    }
+
     return $classes;
 });
